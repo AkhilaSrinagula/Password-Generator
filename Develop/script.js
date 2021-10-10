60a85cbe08e5dc0010d2f0c8aba56e2c81578af6
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-10-10 02:44:38 -0500

    This needs a value prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,60 @@
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var num = '0123456789';
-var sym = '!@#$%^&*()';
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var sym = "!@#$%^&*()";
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var characters = document.getElementById("characters");
 
-
 // Write password to the #password input
-function writePassword(passwordLength,characters) {
-var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var password = '';
-var passwordLength = 8;
+function writePassword() {
+  var characters =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var password = "";
+  var passwordLength = 8;
 
-enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-if (!enter) {
-  alert("This needs a value");
-} else if (enter < 8 || enter > 128) {
-  // Validates user input
-  // Start user input prompts
-  enter = parseInt(prompt("You must choose between 8 and 128"));
-} else {
-  // Continues once user input is validated
-  num = confirm("Will this contain numbers?");
-  sym = confirm("Will this contain special characters?");
-  upperCase = confirm("Will this contain Uppercase letters?");
-  lowerCase = confirm("Will this contain Lowercase letters?");
-};
+  var enter = parseInt(
+    prompt(
+      "How many characters would you like your password? Choose between 8 and 128"
+    )
+  );
+  if (!enter) {
+    alert("This needs a value");
+  } else if (enter < 8 || enter > 128) {
+    // Validates user input
+    // Start user input prompts
+    enter = parseInt(prompt("You must choose between 8 and 128"));
+  } else {
+    // Continues once user input is validated
+    passwordLength = enter - 1;
+    var useNum = confirm("Will this contain numbers?");
+    if (!useNum) {
+      characters = characters.replace(/[0-9]/g, "");
+    }
+    var useSym = confirm("Will this contain special characters?");
+    if (!useSym) {
+      characters = characters.replace(/[^\w\s]/gi, "");
+    }
+    var useUpperCase = confirm("Will this contain Uppercase letters?");
+    if (!useUpperCase) {
+      characters = characters.replace(/[A-Z]/g, "");
+    }
+    var useLowerCase = confirm("Will this contain Lowercase letters?");
+    if (!useLowerCase) {
+      characters = characters.replace(/[a-z]/g, "");
+    }
 
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * characters.length);
-  password += characters.substring(randomNumber, randomNumber +1);
- }
+    for (var i = 0; i <= passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * characters.length);
+      password += characters.substring(randomNumber, randomNumber + 1);
+    }
 
- document.getElementById("password").value = password;
-
+    if (password.length === 0) {
+      alert("You must pick 1 option");
+    } else {
+      document.getElementById("password").value = password;
+    }
+  }
 }
-
-  
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Password-Generator
+Password Generator emphasizes the use of Javascript to generate a random, secure password for the user.
+
+This Project has been deployed to GitHub pages, and has been build with 
+
+HTML
+
+CSS and
+
+Javascript
+
+
+# Getting Started
+To get this project up and running, please follow the links below
+
+URL Link:
+
+Repo Link:
+
+HTML,CSS and Javascript helps to create a random password generator
+This emphasizes the use of Javascript to make dynamic changes. which has helped in creating and making it work fine.
+
+The user will be prompted to choose the  criteria between 8 and 128 characters.
+When prompted for character types then the user can choose lowercase, uppercase, numeric, and/or special characters.
+And finally when all the prompts are answered there is a unique password generated which can be copied from the page.
+
+# This Project has following features:
+
+A generate button which will send a series of prompts.
+ 
+ ![Webpage with the prompts](assets/Webpage.png)
+
+
+And when all the information is collected a unique password is created and is displayed in the box.
+
+![Webpage with the prompts](assets/Webpage2.png)
